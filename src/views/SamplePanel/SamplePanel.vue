4f5985d51201d4114af679bdbfdb9545a3dd02ed
Hakan ÖZGÜR <hakan.ozgur@gmail.com>
2021-08-10 17:26:42 +0000

    small changes

---
+++ src/views/SamplePanel/SamplePanel.vue
@@ -0,0 +1,21 @@
+<template>
+    <m-card>
+        <h4>Sample Panel</h4>
+    </m-card>
+</template>
+
+<script>
+
+export default {
+    name: 'SamplePanel',
+    data() {
+        return {
+        }
+    },
+    methods: {
+        OnEnter(e) {
+            console.log(e);
+        }
+    }
+}
+</script>
